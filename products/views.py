a08d647a83956a5f6df5f737440b23d1f710c66d
Blommis <blomquist799@gmail.com>
2025-06-13 16:52:53 +0200

    added paths to supplements url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Supplement
 # Create your views here.
+
+
+def supplements(request):
+    """A view to show the supplement page"""
+
+    supplements = Supplement.objects.all()
+
+    context = {
+        'supplements': supplements, 
+    }
+    return render(request, 'products/supplements.html', context)
