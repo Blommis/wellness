6772e7459e01978db51353ba8053ad962d67db58
Blommis <blomquist799@gmail.com>
2025-06-13 15:12:33 +0200

    created supplement model and fixtures

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Supplement(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    sku = models.CharField(max_length=50, unique=True)
+    image = models.ImageField(upload_to='media/')
+
+    def __str__(self):
+        return self.name
