b937a54be8964773741491b18db9860e896b80f1
Blommis <blomquist799@gmail.com>
2025-06-16 13:18:42 +0200

    created search functioallity

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Supplement
 # Create your views here.
 
@@ -7,9 +9,21 @@ def supplements(request):
     """A view to show the supplement page"""
 
     supplements = Supplement.objects.all()
+    query = None
+
+    if request.GET: 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Coulndt find your wishing product")
+                return redirect(reverse('supplements'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            supplements = supplements.filter(queries)
 
     context = {
-        'supplements': supplements, 
+        'supplements': supplements,
+        'search_term': query,
     }
     return render(request, 'products/supplements.html', context)
 
