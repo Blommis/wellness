60f1a5f5fe79a42322ec2a2d4c08cd11066ed82a
Blommis <blomquist799@gmail.com>
2025-06-16 12:51:04 +0200

    created path link so the product user clicks on appear

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.supplements, name='supplements')
+    path('', views.supplements, name='supplements'),
+    path('<int:pk>/', views.supplement_detail, name='supplement_detail')
 ]
