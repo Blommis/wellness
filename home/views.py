ce57aa5bda477a98d33de4036ab89abd96448924
Blommis <blomquist799@gmail.com>
2025-06-16 14:43:27 +0200

    cretaed event model and applied in index.html

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Event
+from datetime import date
 # Create your views here.
 
 
 def index(request):
-    """View return the index page"""
-
-    return render(request, 'home/index.html')
+    """View to return the index page with upcoming events"""
+    events = Event.objects.filter(date__gte=date.today()).order_by('date')
+    return render(request, 'home/index.html', {'events': events})
