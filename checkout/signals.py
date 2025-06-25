2ca9a4fb3d169377a3f89286b259f0877de3b813
Blommis <blomquist799@gmail.com>
2025-06-25 12:01:33 +0200

    Add signals to update order total on OrderLineItem save and delete

---
+++ checkout/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update the order total when a line item is created or updated.
+    """
+    if created:
+        print(f"New line item created for order: {instance.order.order_number}")
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update the order total when a line item is deleted.
+    """
+    instance.order.update_total()
