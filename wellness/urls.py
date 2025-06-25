b3a3d85e5b44727a63e3d5fdfd9bfc2953d3e890
Blommis <blomquist799@gmail.com>
2025-06-25 13:41:54 +0200

    Add checkout URL to project and app-level URL configurations

--- wellness/urls.py
+++ wellness/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include(('products.urls', 'products'), namespace='products')),     
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
