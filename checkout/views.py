f2ca6782481c99e6c4850710be579f2af144675f
Blommis <blomquist799@gmail.com>
2025-06-26 12:04:24 +0200

    Update checkout view to include bag context for order summary display

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
+from bag.context_processors import bag_contents
 
 # Create your views here.
 
@@ -12,9 +13,16 @@ def checkout(request):
         return redirect(reverse('home'))
 
     order_form = OrderForm()
+
+    bag_data = bag_contents(request)
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'bag_items': bag_data['bag_items'],
+        'order_total': bag_data['total'],
+        'delivery_cost': bag_data['delivery'],
+        'grand_total': bag_data['grand_total'],
     }
 
     return render(request, template, context)
