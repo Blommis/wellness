2607dd933715d57e35912f810e71d42fcf3804c5
Blommis <blomquist799@gmail.com>
2025-07-01 13:29:20 +0200

    Integrate Stripe PaymentIntent logic into checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -2,19 +2,31 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
 from bag.context_processors import bag_contents
-
+from django.conf import settings
+import stripe
 # Create your views here.
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Unfortunately, your bag is empty.")
         return redirect(reverse('home'))
 
-    order_form = OrderForm()
-
     bag_data = bag_contents(request)
+    total = bag_data['grand_total']
+    stripe_total = round(total * 100)
+    
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    order_form = OrderForm()
 
     template = 'checkout/checkout.html'
     context = {
@@ -23,6 +35,8 @@ def checkout(request):
         'order_total': bag_data['total'],
         'delivery_cost': bag_data['delivery'],
         'grand_total': bag_data['grand_total'],
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
