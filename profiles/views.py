7a3b2ab73e13b3183c7809bdac3cef77e0adb62d
Blommis <blomquist799@gmail.com>
2025-07-03 16:31:45 +0200

    Add profile view logic to update and display user shipping details

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+from django.contrib import messages
 # Create your views here.
 
 
+@login_required
 def profile(request):
-    """ Shows the users profile """
-    template = 'profiles/profile.html'
-    context = {}
+    """ Shows the users profile and update detail information """
+    profile = request.user.userprofile
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile was updated successfully!')
+    else:
+        form = UserProfileForm(instance=profile)
+    
+    context = {
+        'form': form,
+    }
+
+    template = 'profiles/profile.html'
+    
     return render(request, template, context)
 
