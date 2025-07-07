488a54b9f7a8640e2177256e0420d9e5b6a08d75
Blommis <blomquist799@gmail.com>
2025-07-07 12:30:39 +0200

    Update views to display breakfast, lunch, and snack recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import Breakfast, Lunch, Snack
 # Create your views here.
 
 
 def recipes(request):
-    return render(request, 'recipes/recipes.html')
+    breakfasts = Breakfast.objects.all()
+    lunches = Lunch.objects.all()
+    snacks = Snack.objects.all()
+
+    context = {
+        'breakfasts': breakfasts,
+        'lunches': lunches,
+        'snacks': snacks,
+    }
+    return render(request, 'recipes/recipes.html', context)
