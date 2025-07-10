703e6fdb24cb27c3bcaefbfaf1cb3acc35051fae
Blommis <blomquist799@gmail.com>
2025-07-10 12:09:48 +0200

    Include full billing and shipping data in confirmCardPayment call

--- static/js/script.js
+++ static/js/script.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener('DOMContentLoaded', function(){
   const stripePublicKey = document.getElementById('id_stripe_public_key').textContent.trim();
-  const clientSecret = document.getElementById('id_client_secret').textContent.trim();
+  const clientSecret = document.getElementById('id_client_secret').value.trim();
   const stripe = Stripe(stripePublicKey);
   const elements = stripe.elements();
 
@@ -93,28 +93,63 @@ document.addEventListener('DOMContentLoaded', function(){
     card.update({ 'disabled': true });
     submitButton.disabled = true;
 
+    const fullName = document.querySelector('input[name="full_name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const phone = document.querySelector('input[name="phone_number"]').value;
+    const addressLine1 = document.querySelector('input[name="street_address1"]').value;
+    const addressLine2 = document.querySelector('input[name="street_address2"]').value;
+    const city = document.querySelector('input[name="town_or_city"]').value;
+    const state = document.querySelector('input[name="county"]').value;
+    const postalCode = document.querySelector('input[name="postcode"]').value;
+    const country = document.querySelector('select[name="country"]').value;
+
     stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: card,
-      }
-    }).then(function (result) {
-
-      if (result.error) {
-        const errorDiv = document.getElementById("card-errors");
-        errorDiv.innerHTML = `
-          <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-          </span>
-          <span>${result.error.message}</span>
-        `;
-        card.update({ 'disabled': false });
-        submitButton.disabled = false;
-      } else {
-        if (result.paymentIntent.status === "succeeded") {
-          form.submit();
+        billing_details: {
+          name: fullName,
+          email: email,
+          phone: phone,
+          address: {
+            line1: addressLine1,
+            line2: addressLine2,
+            city: city,
+            state: state,
+            postal_code: postalCode,
+            country: country,
+          }
+        }
+      },
+      shipping: {
+        name: fullName,
+        phone: phone,
+        address: {
+          line1: addressLine1,
+          line2: addressLine2,
+          city: city,
+          state: state,
+          postal_code: postalCode,
+          country: country,
         }
       }
-    });
+   }).then(function (result) {
+
+    if (result.error) {
+      const errorDiv = document.getElementById("card-errors");
+      errorDiv.innerHTML = `
+        <span class="icon" role="alert">
+          <i class="fas fa-times"></i>
+        </span>
+        <span>${result.error.message}</span>
+      `;
+      card.update({ 'disabled': false });
+      submitButton.disabled = false;
+    } else {
+      if (result.paymentIntent.status === "succeeded") {
+        form.submit();
+      }
+    }
+  });
   });
 });
 
