a77491f65077de35c1841d1a645ecb6bf43bf920
Blommis <blomquist799@gmail.com>
2025-07-16 13:58:00 +0200

    removed unused imports and fixed whitespaces and long lines

--- wellness/urls.py
+++ wellness/urls.py
@@ -18,18 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('', include(('products.urls', 'products'), namespace='products')),     
+    path('', include(('products.urls', 'products'), namespace='products')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('recipes/', include('recipes.urls')),
     path('about/', include('about.urls')),
-] 
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
