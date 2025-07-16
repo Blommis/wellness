99999186fcc3a863774705264ba9b2b992caf023
Blommis <blomquist799@gmail.com>
2025-07-16 13:50:04 +0200

    fixed lines too long and added noqa due 'false positive warnings'

--- wellness/settings.py
+++ wellness/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-    import env
+    """ noqa is added to avoid false positive linting warnings. """
+    import env  # noqa:
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+zfbz@yts_va1a#_x8=&locy90tm4vndox-3c^j1s4@q3mhp3'
+SECRET_KEY = 'django-insecure--+zfbz@yts_va1a#_x8=&locy90tm4vndox-3c^j1s4@'\
+             'q3mhp3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -98,7 +100,7 @@ TEMPLATES = [
                 'crispy_forms.templatetags.crispy_forms_tags',
                 'crispy_forms.templatetags.crispy_forms_field',
             ],
-            
+
         },
     },
 ]
@@ -146,16 +148,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
